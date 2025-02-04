c09956a687db76e97a790313084a8f0007eb6fc7
gofrywplynie <dawidmarkiewicz214@gmail.com>
2025-02-04 13:11:08 +0100

    added user validation on buisness layer

--- internal/services/services.go
+++ internal/services/services.go
@@ -1,13 +1,59 @@
 package services
 
-import "projekt/internal/repository"
+import (
+	"errors"
+	"projekt/internal/models"
+	"projekt/internal/repository"
+	"slices"
+	"time"
+)
 
 type BuisnessLayer struct {
 	repo *repository.Repository
 }
 
+var availableGroups = []string{"admin", "premium", "user"}
+
 func New(repo *repository.Repository) *BuisnessLayer {
 	return &BuisnessLayer{
 		repo: repo,
 	}
+}
+
+func CheckUserGroup(user models.Request) bool {
+	if user.Group == nil || !slices.Contains(availableGroups, *user.Group) {
+		return false
+	}
+
+	return true
+}
+
+func CalculateAge(user models.Request) int {
+	currentYear := time.Now().Year()
+
+	age := currentYear - *user.BirthYear
+
+	return age
+}
+
+func MapRequestToUser(userRequest models.Request) (models.User, error) {
+	user := models.User{}
+	if userRequest.FirstName == nil {
+		return models.User{}, errors.New("no firstName provided")
+	}
+	user.FirstName = *userRequest.FirstName
+	if userRequest.LastName == nil {
+		return models.User{}, errors.New("no lastName provided")
+	}
+	user.LastName = *userRequest.LastName
+	if userRequest.BirthYear == nil {
+		return models.User{}, errors.New("no birthYear provided")
+	}
+	user.Age = CalculateAge(userRequest)
+	if !CheckUserGroup(userRequest) {
+		return models.User{}, errors.New("no group or wrong group provided")
+	}
+	user.Group = *userRequest.Group
+
+	return user, nil
 }
--- internal/services/services_test.go
+++ internal/services/services_test.go
@@ -1,9 +1,44 @@
 package services
 
 import (
+	"projekt/internal/models"
 	"testing"
 )
 
 func TestAgeCalculation(t *testing.T) {
+	birthYear := 2000
+	user := models.Request{
+		BirthYear: &birthYear,
+	}
+	expectedAge := 25
 
+	calculatedAge := CalculateAge(user)
+	if calculatedAge != expectedAge {
+		t.Errorf("expected %v but got %v", expectedAge, calculatedAge)
+	}
+}
+
+func TestGroupCheck(t *testing.T) {
+	group := "admin"
+	user := models.Request{
+		Group: &group,
+	}
+	if !CheckUserGroup(user) {
+		t.Errorf("%v expected true got false", group)
+	}
+	group = "premium"
+	user.Group = &group
+	if !CheckUserGroup(user) {
+		t.Errorf("%v expected true got false", group)
+	}
+	group = "user"
+	user.Group = &group
+	if !CheckUserGroup(user) {
+		t.Errorf("%v expected true got false", group)
+	}
+	group = "adawds"
+	user.Group = &group
+	if CheckUserGroup(user) {
+		t.Errorf("%v expected false got true", group)
+	}
 }
