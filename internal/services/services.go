8d89b9fd056c7547a742bf67f9833a5d7023da35
gofrywplynie <dawidmarkiewicz214@gmail.com>
2025-02-05 11:37:11 +0100

    buisness layer + testy

--- internal/services/services.go
+++ internal/services/services.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"projekt/internal/models"
 	"projekt/internal/repository"
+	"reflect"
 	"slices"
 	"time"
 )
@@ -21,11 +22,7 @@ func New(repo *repository.Repository) *BuisnessLayer {
 }
 
 func CheckUserGroup(user models.Request) bool {
-	if user.Group == nil || !slices.Contains(availableGroups, *user.Group) {
-		return false
-	}
-
-	return true
+	return slices.Contains(availableGroups, *user.Group)
 }
 
 func CalculateAge(user models.Request) int {
@@ -36,34 +33,34 @@ func CalculateAge(user models.Request) int {
 	return age
 }
 
-func MapRequestToUser(userRequest models.Request) (models.User, error) {
-	user := models.User{}
-	if userRequest.FirstName == nil {
-		return models.User{}, errors.New("no firstName provided")
+func CheckAllFieldsProvided(userRequest models.Request) bool {
+	userRequestFields := reflect.ValueOf(userRequest)
+	for i := 0; i < userRequestFields.NumField(); i++ {
+		field := userRequestFields.Field(i)
+		if field.IsNil() {
+			return false
+		}
 	}
-	user.FirstName = *userRequest.FirstName
-	if userRequest.LastName == nil {
-		return models.User{}, errors.New("no lastName provided")
-	}
-	user.LastName = *userRequest.LastName
-	if userRequest.BirthYear == nil {
-		return models.User{}, errors.New("no birthYear provided")
-	}
-	user.Age = CalculateAge(userRequest)
-	if !CheckUserGroup(userRequest) {
-		return models.User{}, errors.New("no group or wrong group provided")
-	}
-	user.Group = *userRequest.Group
-
-	return user, nil
+	return true
 }
 
 func (b *BuisnessLayer) AddUser(userRequest models.Request) error {
-	user, err := MapRequestToUser(userRequest)
-	if err != nil {
-		return err
+	if !CheckAllFieldsProvided(userRequest) {
+		return errors.New("provide all fields")
+	}
+
+	if !CheckUserGroup(userRequest) {
+		return errors.New("wrong group provided")
+	}
+
+	user := models.User{
+		FirstName: *userRequest.FirstName,
+		LastName:  *userRequest.LastName,
+		Age:       CalculateAge(userRequest),
+		Group:     *userRequest.Group,
 	}
 	b.repo.AddUser(user)
+
 	return nil
 }
 
@@ -77,4 +74,27 @@ func (b *BuisnessLayer) GetUser(id int) (models.User, error) {
 
 func (b *BuisnessLayer) DeleteUser(id int) error {
 	return b.repo.DeleteUser(id)
+}
+
+func (b *BuisnessLayer) UpdateUser(userRequest models.Request, id int) error {
+	user := models.User{}
+
+	if userRequest.Group != nil {
+		if !CheckUserGroup(userRequest) {
+			return errors.New("wrong group provided")
+		}
+		user.Group = *userRequest.Group
+	}
+	if userRequest.BirthYear != nil {
+		user.Age = CalculateAge(userRequest)
+	}
+	if userRequest.FirstName != nil {
+		user.FirstName = *userRequest.FirstName
+	}
+	if userRequest.LastName != nil {
+		user.LastName = *userRequest.LastName
+	}
+
+	err := b.repo.UpdateUser(user, id)
+	return err
 }
--- internal/services/services_test.go
+++ internal/services/services_test.go
@@ -2,8 +2,26 @@ package services
 
 import (
 	"projekt/internal/models"
+	"projekt/internal/repository"
 	"testing"
 )
+
+func CreateRepo() *repository.Repository {
+	user := models.User{
+		Id:        1,
+		FirstName: "Dawid",
+		LastName:  "Markiewicz",
+		Age:       16,
+		Group:     "admin",
+	}
+
+	return &repository.Repository{
+		Users: map[int]models.User{
+			1: user,
+		},
+		NextId: 2,
+	}
+}
 
 func TestAgeCalculation(t *testing.T) {
 	birthYear := 2000
@@ -41,4 +59,79 @@ func TestGroupCheck(t *testing.T) {
 	if CheckUserGroup(user) {
 		t.Errorf("%v expected false got true", group)
 	}
+}
+
+func CreateExampleUserRequest() *models.Request {
+	firstName := "Dawid"
+	lastName := "Markiewicz"
+	birthYear := 2009
+	group := "admin"
+
+	return &models.Request{
+		FirstName: &firstName,
+		LastName:  &lastName,
+		BirthYear: &birthYear,
+		Group:     &group,
+	}
+}
+
+func TestAddUser(t *testing.T) {
+	repo := repository.New()
+	bLayer := New(repo)
+	user := CreateExampleUserRequest()
+
+	err := bLayer.AddUser(*user)
+
+	if err != nil {
+		t.Errorf("error encountered: %v", err)
+	}
+}
+
+func TestRaiseWhenNotAllFieldsProvided(t *testing.T) {
+	repo := repository.New()
+	bLayer := New(repo)
+	user := CreateExampleUserRequest()
+	user.FirstName = nil
+
+	err := bLayer.AddUser(*user)
+
+	if err == nil {
+		t.Error("should throw error")
+	}
+}
+
+func TestRaiseWhenWrongGroupProvided(t *testing.T) {
+	repo := repository.New()
+	bLayer := New(repo)
+	user := CreateExampleUserRequest()
+	group := "adawds"
+	user.Group = &group
+
+	err := bLayer.AddUser(*user)
+
+	if err == nil {
+		t.Error("should throw error")
+	}
+}
+
+func TestUpdateUser(t *testing.T) {
+	repo := CreateRepo()
+	bLayer := New(repo)
+	user := CreateExampleUserRequest()
+	birthYear := 2010
+
+	user.BirthYear = &birthYear
+	user.FirstName = nil
+	user.LastName = nil
+	user.Group = nil
+
+	err := bLayer.UpdateUser(*user, 1)
+	if err != nil {
+		t.Errorf("error hrown: %v", err)
+	}
+
+	if repo.Users[1].Age != 15 {
+		t.Errorf("Expected age 15 but got %v instead", repo.Users[1].Age)
+	}
+
 }
